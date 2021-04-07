33a3d8f77a1367926b656a561bdf020662a07f1f
전철호 <cheolho@wisoft.io>
2021-04-07 17:30:34 +0900

    Add Examples of 3.6. Data Classes and 3.2 Named & Default Arguments

---
+++ src/main/kotlin/chap3/NamedDefaultArgs/Color3.kt
@@ -0,0 +1,15 @@
+package chap3.NamedDefaultArgs.color3
+
+import atomictest.eq
+
+class Color (
+  val red: Int = 0,
+  val green: Int = 0,
+  val blue: Int = 0,
+) {
+  override fun toString() = "($red, $green, $blue)"
+}
+
+fun main() {
+  Color(red = 77).toString() eq "(77, 0, 0)"
+}
