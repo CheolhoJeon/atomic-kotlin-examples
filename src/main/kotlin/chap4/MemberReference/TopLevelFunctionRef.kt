839d4468c22d65595430fb693ec08e2f4483dcaf
전철호 <cheolho@wisoft.io>
2021-04-20 17:17:14 +0900

    Add Examples of 4.3

---
+++ src/main/kotlin/chap4/MemberReference/TopLevelFunctionRef.kt
@@ -0,0 +1,18 @@
+package chap4.MemberReference.memberreferrence2
+
+import atomictest.eq
+
+fun ignore(message: Message) =
+  !message.isImportant() &&
+      message.sender in setOf("Boss", "Mom")
+
+fun main() {
+  val text = "Let's discuss goals " +
+      "for the next year"
+  val msgs = listOf(
+    Message("Boss", text, false, listOf()),
+    Message("Boss", text, false, listOf(
+      Attachment("image", "cute cats"))))
+  msgs.filter(::ignore).size eq 1
+  msgs.filterNot(::ignore).size eq 1
+}
