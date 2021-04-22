8210ab9d2c1468ad4f0f9f601d3d66c069386903
전철호 <cheolho@wisoft.io>
2021-04-22 17:10:35 +0900

    Add Examples of 4.6
    * Flatten.kt
    * FlattenAndFlatMap.kt
    * PlayingCards.kt
    * WhyFlatMap.kt
    * ZipAndTransform.kt
    * Zipper.kt
    * ZippingWithNext.kt
    
    Resolves: #29
    See also: #issueNo

---
+++ src/main/kotlin/chap4/ManipulatingLists/Flatten.kt
@@ -0,0 +1,12 @@
+package chap4.ManipulatingLists
+
+import atomictest.eq
+
+fun main() {
+  val list = listOf(
+    listOf(1, 2),
+    listOf(4, 5),
+    listOf(7, 8),
+  )
+  list.flatten() eq "[1, 2, 4, 5, 7, 8]"
+}
---
+++ src/main/kotlin/chap4/ManipulatingLists/ZippingWithNext.kt
@@ -0,0 +1,15 @@
+package chap4.ManipulatingLists
+
+import atomictest.eq
+
+fun main() {
+  val list = listOf('a', 'b', 'c', 'd')
+
+  list.zipWithNext() eq listOf(
+    Pair('a', 'b'),
+    Pair('b', 'c'),
+    Pair('c', 'd'))
+
+  list.zipWithNext { a, b ->  "$a$b"} eq
+      "[ab, bc, cd]"
+}
